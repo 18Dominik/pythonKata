0d212b8c1310f0066399210615a1a4dfe5588979
Dominik <35842490+18Dominik@users.noreply.github.com>
2023-08-15 21:53:53 +0200

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,8 +6,7 @@ def count_ips(filename):
     with open(filename, 'r') as file: 
         myIps_list = [line.strip().split('- -')[0] for line in file] # For-Loop in one line: List Comprehension Syntax (https://www.w3schools.com/python/python_lists_comprehension.asp, https://betterprogramming.pub/4-ways-to-write-one-liner-for-loops-in-python-e8c1db903ce2). Get IPs from file
         myIps_set = set(myIps_list) #gives unique values
-        my_dict = {i:myIps_list.count(i) for i in myIps_list} # For-Loop in one line: Dictionary Comprehensions Syntax, counts number of ip calls per ip adress and assigns count-value to ip-address key (https://www.geeksforgeeks.org/python-dictionary-comprehension/, https://betterprogramming.pub/4-ways-to-write-one-liner-for-loops-in-python-e8c1db903ce2)
-
+        my_dict = {i:myIps_list.count(i) for i in myIps_list} # For-Loop in one line: Dictionary Comprehensions Syntax (https://www.geeksforgeeks.org/python-dictionary-comprehension/, https://betterprogramming.pub/4-ways-to-write-one-liner-for-loops-in-python-e8c1db903ce2), counts number of ip calls per ip adress and assigns count-value to ip-address key 
         return my_dict
 
 # only run the code inside the if statement when the program is run directly by the Python interpreter. 
